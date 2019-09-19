118c3c105049a69d71ce1d8dd4ade42499026498
Luke <lukebenjaminely@gmail.com>
2019-09-19 14:38:44 +0100

    Updated exec to not fail on exit code 2

--- src/exec.js
+++ src/exec.js
@@ -3,7 +3,11 @@ const { exec } = require('child_process')
 module.exports = async (cmd) => new Promise((resolve, reject) => {
   exec(cmd, (err, stdout, stderr) => {
     if (err) {
-      reject(err)
+      if (err.code === 2) {
+        resolve({ stdout, stderr })
+      } else {
+        reject(err)
+      }
     } else {
       resolve({ stdout, stderr })
     }
