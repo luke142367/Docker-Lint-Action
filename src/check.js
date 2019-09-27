0560e220f2b8a6e936150ec49efcb3233e2a19ff
Luke <lukebenjaminely@gmail.com>
2019-09-27 20:13:25 +0100

    Change to use octokit

--- src/check.js
+++ src/check.js
@@ -1,4 +1,4 @@
-const request = require('./request')
+const Octokit = require('@octokit/rest')
 
 const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN } = process.env
 const event = require(GITHUB_EVENT_PATH)
@@ -10,44 +10,31 @@ const { name: repo } = repository
 
 const checkName = 'Docker Lint Check'
 
-const headers = {
-  'Content-Type': 'application/json',
-  Accept: 'application/vnd.github.antiope-preview+json',
-  Authorization: `Bearer ${GITHUB_TOKEN}`,
-  'User-Agent': 'docker-lint-action',
-}
+const octokit = new Octokit({
+  auth: GITHUB_TOKEN,
+})
 
 async function createCheck() {
-  const body = {
+  const check = await octokit.checks.create({
+    owner,
+    repo,
     name: checkName,
     head_sha: GITHUB_SHA,
     status: 'in_progress',
     started_at: new Date(),
-  }
-
-  const { data } = await request(`https://api.github.com/repos/${owner}/${repo}/check-runs`, {
-    method: 'POST',
-    headers,
-    body,
   })
-  const { id } = data
-  return id
+  return check.id
 }
 
 async function updateCheck(id, conclusion, output) {
-  const body = {
-    name: checkName,
-    head_sha: GITHUB_SHA,
+  await octokit.checks.update({
+    owner,
+    repo,
+    check_run_id: id,
     status: 'completed',
     completed_at: new Date(),
     conclusion,
     output,
-  }
-
-  await request(`https://api.github.com/repos/${owner}/${repo}/check-runs/${id}`, {
-    method: 'PATCH',
-    headers,
-    body,
   })
 }
 
