0ea8b8264d69656650b6a33bc3771a01886b5f73
Luke <lukebenjaminely@gmail.com>
2019-09-28 19:43:31 +0100

    Remove date params

--- src/check.ts
+++ src/check.ts
@@ -31,7 +31,6 @@ async function createCheck() {
     head_sha: GITHUB_SHA,
     name: checkName,
     status: 'in_progress',
-    started_at: new Date().toISOString(),
   })
   return check.data.id
 }
@@ -43,7 +42,6 @@ async function updateCheck(id : number,
     repo,
     check_run_id: id,
     status: 'completed',
-    completed_at: new Date().toISOString(),
     conclusion,
   }
 
